2d34bd04874b32a6409adb8675878366ad5ff036
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-04-20 20:02:59 -0300

    att atividade professor

--- app/src/main/java/com/example/converterdatas/MainActivity.kt
+++ app/src/main/java/com/example/converterdatas/MainActivity.kt
@@ -12,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //habilitar campos de entrada e saida
         val txt_Ano = findViewById<EditText>(R.id.txt_Ano)
-        val txt_Mes = findViewById<EditText>(R.id.txt_Ano)
-        val txt_Dia = findViewById<EditText>(R.id.txt_Ano)
+        val txt_Mes = findViewById<EditText>(R.id.txt_Mes)
+        val txt_Dia = findViewById<EditText>(R.id.txt_Dia)
         val txt_Resultado = findViewById<TextView>(R.id.txt_Resultado)
         val btn_Converter = findViewById<Button>(R.id.btn_Converter)
 
@@ -21,20 +21,33 @@ class MainActivity : AppCompatActivity() {
             //validação de dados
             if(txt_Ano.text.isEmpty()){
                 txt_Ano.error = "Digite a quantidade de ano(s)"
-            }else if(txt_Mes.text.isEmpty()){
-                txt_Mes.error  ="Digite a quantidade de mes(es)"
-            }else if(txt_Dia.text.isEmpty()){
-                txt_Dia.error  ="Digite a quantidade de dia(as)"
+            }
+            else if(txt_Mes.text.isEmpty()) {
+                txt_Mes.error = "Digite a quantidade de mes(es)"
+            }
+            else if(txt_Dia.text.isEmpty()){
+                txt_Dia.error = "Digite a quantidade de dia(as)"
             }else{
                 //entrada de dados
                 val ano = txt_Ano.text.toString().toInt()
-                val mes = txt_Ano.text.toString().toInt()
-                val dia = txt_Ano.text.toString().toInt()
-                //processamento
-                val resultado: Int
-                resultado = ano * 360 + mes * 30 + dia
-                //saida
-                txt_Resultado.text = "Voce tem $resultado dia(s)"
+                val mes = txt_Mes.text.toString().toInt()
+                val dia = txt_Dia.text.toString().toInt()
+                if(mes > 12){
+                    txt_Mes.text.clear()
+                    txt_Mes.error = "Digite uma quantidade de mes(es) valida"
+                }else if(dia > 31){
+                    txt_Dia.text.clear()
+                    txt_Dia.error = "Digite uma quantidade de dia(as) valida"
+                }else {
+                    //processamento
+                    val resultado: Int
+                    resultado = ano * 360 + mes * 30 + dia
+                    //saida
+                    txt_Resultado.text = "Voce tem $resultado dia(s)"
+                    txt_Ano.text.clear()
+                    txt_Mes.text.clear()
+                    txt_Dia.text.clear()
+                }
             }
         }
     }
